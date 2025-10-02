6d4b3d7a2b34affa6ec028ff5fa1dd1397ea1ce9
VingDzz <24020366@vnu.edu.vn>
2025-10-02 23:55:37 +0700

    Add files via upload

---
+++ HW1/Introduction.java
@@ -0,0 +1,13 @@
+public class Introduction {
+    public static void main(String[] args) 
+    {
+      
+        System.out.println("Tran Duc Vinh\t24020366\tIT6\tTranDucVinh24020366\t24020366@vnu.edu.vn");
+
+        for (int i = 9; i >= 1; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
